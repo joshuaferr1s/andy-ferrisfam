69d18fb3f12bc70a2b224e8aca972686d7531f59
Joshua Ferris <jajjferris@gmail.com>
2024-04-07 12:38:08 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Confetti } from "svelte-confetti";
+</script>
+
+<main>
+  <Confetti infinite cone x={[1, 2.5]} y={[0.25, 0.75]} size={70} fallDistance=0px colorArray={["url(/plonker.png)"]} />
+  <!-- <Confetti infinite size=30 colorArray={["url(../public/plonker.png)"]} /> -->
+  
+  <h1>Andy Ferris is cool!</h1>
+  <p>Return to Alison Ferris if found in the park</p>
+</main>
